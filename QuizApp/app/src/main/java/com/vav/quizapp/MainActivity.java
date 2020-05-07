7967c75ede1b9a56efc57091c52faab4a009d01b
Vvarga <vasile.varga@endava.com>
2020-05-07 22:09:20 +0300

    added new features

--- QuizApp/app/src/main/java/com/vav/quizapp/MainActivity.java
+++ QuizApp/app/src/main/java/com/vav/quizapp/MainActivity.java
@@ -14,18 +14,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     Button buttonYes, buttonNo;
     ImageButton buttonNext;
-    TextView questionTextView;
-    private int currenQuestionIndex = 0;
+    TextView questionTextView,nextText,totalPoints;
+    private int currenQuestionIndex = 0, totalPointsText=0;
 
     private Question[] questionArray = new Question[] {
-            new Question(R.string.question_amendments, false),
-            new Question(R.string.question_constitution, true),
-            new Question(R.string.question_declaration, true),
-            new Question(R.string.question_independence_rights, true),
-            new Question(R.string.question_religion, true),
-            new Question(R.string.question_government, false),
-            new Question(R.string.question_government_feds, false),
-            new Question(R.string.question_government_senators, true)
+            new Question(R.string.question_class, false),
+            new Question(R.string.question_oop_invention, true),
+            new Question(R.string.question_first_language, false),
+            new Question(R.string.question_additional_feature, true),
+            new Question(R.string.question_oop_implementation, false),
+            new Question(R.string.question_reusability, true),
+            new Question(R.string.question_multiple_inheritance, false),
+            new Question(R.string.question_oop_rules, true)
     };
 
     @Override
@@ -38,7 +38,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonNo = findViewById(R.id.button_false);
         buttonNext = findViewById(R.id.button_next);
         questionTextView = findViewById(R.id.question_text_view);
+        nextText = findViewById(R.id.next_text);
+        totalPoints = findViewById(R.id.total_points);
         questionTextView.setText(questionArray[currenQuestionIndex].getQuestionResId());
+        totalPoints.setText(String.valueOf(totalPointsText));
+
+
+        nextText.setOnClickListener(this);
         buttonNext.setOnClickListener(this);
         buttonNo.setOnClickListener(this);
         buttonYes.setOnClickListener(this);
@@ -47,20 +53,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View view) {
 
+
         switch (view.getId()){
             case R.id.button_false:
-                Toast.makeText(MainActivity.this,"You've clicked FALSE",Toast.LENGTH_SHORT).show();
+                    checkAnswer(false);
             break;
 
             case R.id.button_true:
-                Toast.makeText(MainActivity.this,"You've clicked TRUE",Toast.LENGTH_SHORT).show();
+                    checkAnswer(true);
                 break;
-            case R.id.button_next:
-                Log.d("CURRENT", "index: "+ currenQuestionIndex);
-                questionTextView.setText(questionArray[currenQuestionIndex].getQuestionResId());
+            case R.id.button_next: case R.id.next_text:
                 currenQuestionIndex = (currenQuestionIndex+1) % questionArray.length;
+                updateQuestion();
+                if (currenQuestionIndex==0){
+                    totalPoints.setText("0");
+                }
                 break;
 
         }
     }
+
+    private void updateQuestion(){
+        Log.d("CURRENT", "index: "+ currenQuestionIndex);
+        questionTextView.setText(questionArray[currenQuestionIndex].getQuestionResId());
+    }
+
+    private void checkAnswer(boolean userChoseCorrect){
+        boolean answerIsTrue = questionArray[currenQuestionIndex].isAnswerTrue();
+
+        int toastMessageId;
+        if (userChoseCorrect == answerIsTrue){
+            totalPointsText+=10;
+            totalPoints.setText(String.valueOf(totalPointsText));
+            toastMessageId = R.string.correct_answer;
+        }
+        else {
+            toastMessageId = R.string.wrong_answer;
+        }
+
+        Toast.makeText(MainActivity.this, toastMessageId, Toast.LENGTH_SHORT).show();
+    }
+
 }
